f9a691e77c0242e1e634050e4a034a2d02b3531c
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-05 15:20:19 +0000

    [feat]: Added UI and function for getting json data

--- app/src/main/java/tech/peny/newsreader/MainActivity.java
+++ app/src/main/java/tech/peny/newsreader/MainActivity.java
@@ -2,13 +2,82 @@ package tech.peny.newsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Config listView
+        ListView listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,titles);
+        listView.setAdapter(arrayAdapter);
+
+
+//        Config
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+//    Async
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpsURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpsURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                Log.i("URL CONTENT", result);
+
+                return result;
+
+            }catch (Exception e){
+
+            }
+            return null;
+        }
     }
 }
